b29890ef468f6bf18180409a638765bcc4f909c5
Vladislav-zv <71664237+Vladislav-zv@users.noreply.github.com>
2026-05-04 02:47:23 +0300

    Update script.js

--- components/script.js
+++ components/script.js
@@ -7,7 +7,16 @@ const componentMap = {
   'my-accordion-3': './components/accordions/my-accordion-3.html',
   'my-accordion-4': './components/accordions/my-accordion-4.html',
   'my-accordion-5': './components/accordions/my-accordion-5.html',
-  'my-accordion-6': './components/accordions/my-accordion-6.html'
+  'my-accordion-6': './components/accordions/my-accordion-6.html',
+  'my-tab-1-ua': './components/navTabs/my-tab-1-ua.html',
+  'my-tab-2-ua': './components/navTabs/my-tab-2-ua.html',
+  'my-tab-3-ua': './components/navTabs/my-tab-3-ua.html',
+  'my-accordion-1-ua': './components/accordions/my-accordion-1-ua.html',
+  'my-accordion-2-ua': './components/accordions/my-accordion-2-ua.html',
+  'my-accordion-3-ua': './components/accordions/my-accordion-3-ua.html',
+  'my-accordion-4-ua': './components/accordions/my-accordion-4-ua.html',
+  'my-accordion-5-ua': './components/accordions/my-accordion-5-ua.html',
+  'my-accordion-6-ua': './components/accordions/my-accordion-6-ua.html'
 };
 
 function createComponent(path) {
